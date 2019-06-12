b4ca5d8dc7e423206d2d8be001a0c8831279ce32
loirou <loirou@163.com>
2019-06-12 17:41:02 +0800

    restore main

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,9 +2,6 @@ package com.github.hcsp;
 
 import org.apache.commons.lang3.StringUtils;
 
-/**
- * @author loirou
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Empty string is empty: " + StringUtils.isEmpty(""));
