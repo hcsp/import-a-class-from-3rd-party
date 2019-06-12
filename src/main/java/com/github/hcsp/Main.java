3080fa3d60da0fee412310442ae9155128347819
loirou <loirou@163.com>
2019-06-12 17:25:13 +0800

    引入第三方包

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,6 +2,9 @@ package com.github.hcsp;
 
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * @author loirou
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Empty string is empty: " + StringUtils.isEmpty(""));
