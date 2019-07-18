4f1b756bc44b2b217cc640c6bf197799cab753c4
Archer <9527.code@gmail.com>
2019-07-18 14:36:41 +0800

    引入第三方

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,7 +3,11 @@ package com.github.hcsp;
 import org.apache.commons.lang3.StringUtils;
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Empty string is empty: " + StringUtils.isEmpty(""));
+
     }
+
 }
