765776ea470c2625d47baa5a24478e542fe95b4a
zhuxueqiao <mya_dong@126.com>
2020-02-14 20:59:01 +0800

    添加第三方包

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Empty string is empty: " + StringUtils.isEmpty(""));
+        System.out.println("Empty string is empty； " + StringUtils.isEmpty(""));
     }
 }
