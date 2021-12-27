01ae7687014b623d16477f90f10af320ec1dc3f0
rico <5924435@qq.com>
2021-12-27 15:36:16 +0800

    完成第三方包的引入，同时恢复main文件秩序

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,11 +2,6 @@ package com.github.hcsp;
 
 import org.apache.commons.lang3.StringUtils;
 
-// serach xxxxx nbsp maven
-// pom.xml 第三方依赖
-
-// 第三方包
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Empty string is empty: " + StringUtils.isEmpty(""));
